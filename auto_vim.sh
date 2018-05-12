689a0568cc1b319652c54dde078921d0c5e1e31b
Alex_Ai <857677909@qq.com>
2018-05-12 10:30:30 +0800

    Delete auto_vim.sh
    这个功能会影响VIM的使用，还是自己手动建立吧

--- auto_vim.sh
+++
@@ -1,5 +0,0 @@
-sudo rm -rf ~/.vim
-mkdir ~/.vim/bundle ~/.vim/vimrcs
-echo "source ~/.vim/vimrcs/bundle" > ~/.vimrc
-echo "source ~/.vim/vimrcs/main" >> ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
